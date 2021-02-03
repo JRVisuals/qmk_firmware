461f5ccd26b7eb0943ba8254fd6c740988ef6ab0
Josker Du <padma@Joskers-Mac-mini.local>
2021-02-03 01:28:03 -0500

    swap cmd and alt again

--- keyboards/planck/keymaps/dr_jrv/keymap.c
+++ keyboards/planck/keymaps/dr_jrv/keymap.c
@@ -21,6 +21,7 @@ enum planck_keycodes {
     DYNAMIC_MACRO_RANGE,
     // Custom Code Macros
     CODE_ARROWFN,
+    CODE_ARROW,
     GIT_ADD,
     GIT_COMMIT,
     GIT_STATUS,
@@ -40,6 +41,12 @@ enum planck_keycodes {
 #define ITERM LGUI(S(KC_I))
 #define SS_SEL LGUI(LCTL(S(KC_4)))
 #define SS_FUL LGUI(S(KC_3))
+
+// Redundant commands on RAISE
+#define UNDO LGUI(KC_Z)
+#define CUT LGUI(KC_X)
+#define COPY LGUI(KC_C)
+#define PASTE LGUI(KC_V)
 
 #ifdef TAP_DANCE_ENABLE
 #    define SFT_CAP TD(0)  // Left shift, double tap for caps
@@ -71,7 +78,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC,
         KC_ESC, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
         SFT_CAP, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, SFT_ENT,
-        MEDIA, KC_LCTL, KC_LCMD, KC_LALT, LOWER, KC_SPC, KC_SPC, RAISE, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT
+        MEDIA, KC_LCTL, KC_LALT, KC_LCMD, LOWER, KC_SPC, KC_SPC, RAISE, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT
         ),
 
     /* NUMPAD
@@ -97,7 +104,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |------+------+------+------+------+------+------+------+------+------+------+------|
      * |      |      |      |SS_FUL|SS_SEL|      |      |   -  |   =  |   [  |   ]  |  \   |
      * |------+------+------+------+------+------+------+------+------+------+------+------|
-     * |LShift|      |      |      |      |      |      |ISO # |ISO / | NULL |   |  | Enter|
+     * |LShift| undo | cut  | copy | paste|      |      |ISO # |ISO / | NULL |   |  | Enter|
      * |------+------+------+------+------+-------------+------+------+------+------+------|
      * |      |      |      |      |      |             |      |      |      |      |      |
      * `-----------------------------------------------------------------------------------'
@@ -105,7 +112,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_RAISE] = LAYOUT_planck_grid(
         KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_DEL,
     _______, _______, _______, SS_FUL, SS_SEL, _______, _______, KC_MINS, KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS,
-    KC_LSFT, _______, _______, _______, _______, _______, _______, KC_NUHS, KC_NUBS, XXXXXXX, KC_PIPE, KC_ENT,
+    KC_LSFT, UNDO, CUT, COPY, PASTE, _______, _______, KC_NUHS, KC_NUBS, XXXXXXX, KC_PIPE, KC_ENT,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
 
@@ -115,7 +122,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |------+------+------+------+------+------+------+------+------+------+------+------|
      * |      |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   _  |   +  |   {  |   }  |  |   |
      * |------+------+------+------+------+------+------+------+------+------+------+------|
-     * |LShift|  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |   =  |   >  | NULL |   \  | Enter|
+     * |LShift|  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |      |  =>  |   [  |   ]  | Enter|
      * |------+------+------+------+------+-------------+------+------+------+------+------|
      * |      |      |      |      |      |             |      | Home | PgDn | PgUp |  End |
      * `-----------------------------------------------------------------------------------'
@@ -123,7 +130,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_LOWER] = LAYOUT_planck_grid(
         KC_TILD, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, LALT(KC_BSPC),
         _______, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
-        KC_LSFT, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_EQL, KC_GT, XXXXXXX, KC_BSLS, KC_ENT,
+        KC_LSFT, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, _______, CODE_ARROW, KC_LBRC, KC_RBRC, KC_ENT,
         _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END
         ),
 
@@ -316,6 +323,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         case CODE_ARROWFN:
             if (record->event.pressed) {
                 SEND_STRING("=()=>{};" SS_TAP(X_LEFT) SS_TAP(X_LEFT) SS_TAP(X_LEFT) SS_TAP(X_LEFT) SS_TAP(X_LEFT) SS_TAP(X_LEFT));
+            }
+            return false;
+            break;
+        case CODE_ARROW:
+            if (record->event.pressed) {
+                SEND_STRING("=> ");
             }
             return false;
             break;
