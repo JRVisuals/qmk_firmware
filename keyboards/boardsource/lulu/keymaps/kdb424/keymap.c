c49c07e4b5602844be39cf6eb4d329c8ae08e9db
Kyle Brown <kdb424@gmail.com>
2022-10-28 16:56:36 -0700

    lulu

--- keyboards/boardsource/lulu/keymaps/kdb424/keymap.c
+++ keyboards/boardsource/lulu/keymaps/kdb424/keymap.c
@@ -1,8 +1,8 @@
-#include QMK_KEYBOARD_H
-#ifdef OLED_DRIVER_ENABLE
-#    include "oled.c"
-#endif
+// Copyright 2022 Cole Smith <cole@boadsource.xyz>
+// SPDX-License-Identifier: GPL-2.0-or-later
+
 #include "kdb424.c"
+#include QMK_KEYBOARD_H
 
 #define _DVORAK 0
 #define _GAMING 1
@@ -11,12 +11,16 @@
 #define _GUI 4
 #define _SYMBOLS 5
 #define REPROGR REPROGRAM_MACRO
+
 // clang-format off
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
 /* DVORAK
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * | Esc  |   '  |   ,  |   .  |   P  |   Y  |                    |   F  |   G  |   C  |   R  |   L  | BKSP |
+ * | Esc  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  | Del  |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |  ~   |   '  |   ,  |   .  |   P  |   Y  |                    |   F  |   G  |   C  |   R  |   L  | BKSP |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | Tab  |   A  |   O  |   E  |   U  |   I  |                    |   D  |   H  |   T  |   N  |   S  |  ENT |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
@@ -26,15 +30,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                          |      |      |/       /         \      \ |      |      |
  *                          `---------------------'           '------''-------------'
  */
- [_DVORAK] = LAYOUT_split_3x6_3( \
-  KC_GESC,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,                      KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_BSPC, \
-  TAB_SB,   KC_A,    KC_O,    KC_E,    KC_U,    KC_I,                      KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_ENT, \
-  OS_LSFT,  KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,                      KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_SLSH, \
-                                      OS_LALT, SUPER_L, LT1_SP,   LT2_SP,  OS_LCTL, KC_NO \
+ [_DVORAK] = LAYOUT( \
+  KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,\
+  KC_TILD, KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,                       KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_BSPC, \
+  TAB_SB,  KC_A,    KC_O,    KC_E,    KC_U,    KC_I,                       KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_ENT, \
+  OS_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    XXXXXXX,  XXXXXXX, KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_SLSH, \
+                             XXXXXXX, OS_LALT, SUPER_L, LT1_SP,   LT2_SP,  OS_LCTL, KC_NO,   XXXXXXX \
 ),
 /* Gaming
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * | Esc  |   '  |   ,  |   .  |   P  |   Y  |                    |   F  |   G  |   C  |   R  |   L  | BKSP |
+ * | Esc  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  | Del  |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |  ~   |   '  |   ,  |   .  |   P  |   Y  |                    |   F  |   G  |   C  |   R  |   L  | BKSP |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | Ctl  |   A  |   O  |   E  |   U  |   I  |                    |   D  |   H  |   T  |   N  |   S  |  ENT |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
@@ -44,14 +51,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                          |      |      |/       /         \      \ |      |      |
  *                          `---------------------'           '------''-------------'
  */
- [_GAMING] = LAYOUT_split_3x6_3( \
-  KC_ESC,   KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,                     KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_BSPC, \
-  KC_LCTL,  KC_A,    KC_O,    KC_E,    KC_U,    KC_I,                     KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_ENT, \
-  KC_LSFT,  KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,                     KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_SLSH, \
-                                    KC_LALT, KC_SPC,  KC_SPC,   LT2_SP,   KC_LCTL, KC_UP \
+ [_GAMING] = LAYOUT( \
+  KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,\
+  KC_TILD, KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,                     KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_BSPC, \
+  KC_LCTL, KC_A,    KC_O,    KC_E,    KC_U,    KC_I,                     KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_ENT, \
+  KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X, _______,  _______,  KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_SLSH, \
+                            _______, KC_LALT, KC_SPC,  KC_SPC,   LT2_SP,  KC_LCTL, KC_UP, _______ \
 ),
 /* RAISE1
  * ,-----------------------------------------.                    ,-----------------------------------------.
+ * | Esc  |      |      |      |      |      |                    |      |      |      |      |      |  Del |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |Reprog|      |      |      |      |      |                    |      |      |   7  |  8   |  9   |  Del |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |      |      |      |      |      |      |                    |      |      |   4  |  5   |  6   |   \  |
@@ -62,11 +72,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                          |      |      |/       /         \      \ |      |      |
  *                          `---------------------'           '------''-------------'
  */
-[_RAISE1] = LAYOUT_split_3x6_3( \
+[_RAISE1] = LAYOUT( \
+  _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______, \
   KC_GRV,  _______, _______, _______, _______, _______,                   XXXXXXX, XXXXXXX, KC_7,    KC_8,    KC_9,    KC_DEL,  \
   _______, _______, _______, _______, KC_TAB,  _______,                   XXXXXXX, XXXXXXX, KC_4,    KC_5,    KC_6,    KC_BSLS, \
-  _______, _______, _______, _______, _______, REPROGR,                   XXXXXXX, XXXXXXX, KC_1,    KC_2,    KC_3,    KC_MINS, \
-                                      _______, _______, _______, _______, KC_EQL,  KC_0 \
+  _______, _______, _______, _______, _______, REPROGR, _______, _______, XXXXXXX, XXXXXXX, KC_1,    KC_2,    KC_3,    KC_MINS, \
+                             _______, _______, _______, _______, _______, KC_EQL,  KC_0, _______ \
 ),
 /* RAISE2
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -81,11 +92,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                          `---------------------'           '------''-------------'
  */
 
-[_RAISE2] = LAYOUT_split_3x6_3( \
+[_RAISE2] = LAYOUT( \
+  _______, _______, _______, _______, _______, _______,                     _______, _______, _______, _______, _______, _______, \
   _______, KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______,                     _______, _______, _______, KC_LBRC, KC_RBRC, LSFT(KC_INS), \
   _______, KC_F5,   KC_F6,   KC_F7,   KC_F8,   _______,                     KC_HOME, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_END,  \
-  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   _______,                     _______, _______, _______, _______, _______, KC_BSLS, \
-                                      KC_LCTL, _______,  _______, _______,  DF(0),   DF(1) \
+  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   _______,  _______, _______,  _______, _______, _______, _______, _______, KC_BSLS, \
+                             _______, KC_LCTL, _______,  _______, _______,  DF(0),   DF(1), _______ \
 ),
 /* GUI
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -99,12 +111,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                          |      |      |/       /         \      \ |      |      |
  *                          `---------------------'           '------''-------------'
  */
-[_GUI] = LAYOUT_split_3x6_3( \
+[_GUI] = LAYOUT( \
+  _______, _______, _______, _______, _______, _______,                     _______, _______, _______, _______, _______, _______, \
   _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                        _______, _______, _______, _______, _______, _______, \
   _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,                        _______, _______, _______, _______, _______, _______, \
-  _______, _______, _______, _______, _______, _______,                     _______, _______, _______, _______, _______, _______, \
-
-                                      _______, _______,  _______, _______,  CMB_ON,  CMB_OFF \
+  _______, _______, _______, _______, _______, _______,  _______, _______,  _______, _______, _______, _______, _______, _______, \
+                             _______, _______, _______,  _______, _______,  CMB_ON,  CMB_OFF, _______ \
 ),
 /* SYMBOLS
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -118,11 +130,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                          |      |      |/       /         \      \ |      |      |
  *                          `---------------------'           '------''-------------'
  */
-[_SYMBOLS] = LAYOUT_split_3x6_3( \
+[_SYMBOLS] = LAYOUT( \
+  _______, _______, _______, _______, _______, _______,                     _______, _______, _______, _______, _______, _______, \
   _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                     KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL, \
   _______, RGB_HUI, RGB_HUD, RGB_VAI, RGB_VAD, RGB_M_SW,                    _______, _______, _______, KC_LBRC, KC_RBRC, _______, \
-  _______, RGB_M_P, RGB_MOD, RGB_SAI, RGB_SAD, RGB_M_TW,                    _______, _______, _______, _______, _______, _______, \
-                                      RGB_TOG, _______,  _______, _______,  _______, _______ \
+  _______, RGB_M_P, RGB_MOD, RGB_SAI, RGB_SAD, RGB_M_TW, _______, _______,  _______, _______, _______, _______, _______, _______, \
+                             _______, RGB_TOG, _______,  _______, _______,  _______, _______, _______ \
 )
 };
-
--- users/kdb424/kdb424.c
+++ users/kdb424/kdb424.c
@@ -11,89 +11,47 @@
 #define SUPER_L LM(_GUI, MOD_LGUI)
 
 enum custom_keycodes {
-  DVORAK = SAFE_RANGE,
-  PLOVER,
-  RAISE1,
-  RAISE2,
-  GUI,
-  SYMBOLS,
-  GAMING,
-  REPROGRAM_MACRO,
+    DVORAK,
+    PLOVER,
+    RAISE1,
+    RAISE2,
+    GUI,
+    SYMBOLS,
+    GAMING,
+    REPROGRAM_MACRO,
 };
 
 #ifdef COMBO_ENABLE
-#include "combos.c"
-#endif  // COMBOS_ENABLE
+#    include "combos.c"
+#endif // COMBOS_ENABLE
 
-#ifdef ENCODER_ENABLE
-bool encoder_update_user(uint8_t index, bool clockwise) {
-    if(IS_LAYER_ON(_RAISE1)) { // on Raise layer control volume
-        if (index == 0) {
-            if (clockwise) {
-                #if defined(RGBLIGHT_ENABLE)
-                rgblight_increase_hue_noeeprom();
-                #endif
-            } else {
-                #if defined(RGBLIGHT_ENABLE)
-                rgblight_decrease_hue_noeeprom();
-                #endif
+#include <stdbool.h>
+#include QMK_KEYBOARD_H // Needed for macros
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    if (record->event.pressed) {
+    }
+    switch (keycode) {
+        case REPROGRAM_MACRO:
+            if (record->event.pressed) {
+                SEND_STRING(SS_LGUI(SS_TAP(X_ENTER)));
+                wait_ms(900);
+                SEND_STRING("cd ~/src/qmk-firmware" SS_TAP(X_ENTER));
+                wait_ms(100);
+                SEND_STRING("sleep 1 && make " QMK_KEYBOARD ":" QMK_KEYMAP ":dfu && exit" SS_TAP(X_ENTER));
+                reset_keyboard();
+                return false;
+                break;
             }
-        } else if (index == 1) {
-            if (clockwise) {
-                #if defined(RGBLIGHT_ENABLE)
-                rgblight_increase_val_noeeprom();
-                #endif
-            } else {
-                #if defined(RGBLIGHT_ENABLE)
-                rgblight_decrease_val_noeeprom();
-                #endif
-            }
-        }
-    } else {
-        if (index == 0) {
-            if (clockwise) {
-                tap_code16(C(KC_R));
-            } else {
-                tap_code(KC_U);
-            }
-        } else if (index == 1) {
-            if (clockwise) {
-                tap_code(KC_WH_D);
-            } else {
-                tap_code(KC_WH_U);
-            }
-        }
     }
     return true;
 }
 
-#endif
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  if (record->event.pressed) {
-  }
-  switch (keycode) {
-    case REPROGRAM_MACRO:
-      if (record->event.pressed) {
-        SEND_STRING(SS_LGUI(SS_TAP(X_ENTER)));
-        wait_ms(900);
-        SEND_STRING("cd ~/src/qmk-firmware" SS_TAP(X_ENTER));
-        wait_ms(100);
-        SEND_STRING("sleep 1 && make "QMK_KEYBOARD":"QMK_KEYMAP":dfu && exit" SS_TAP(X_ENTER));
-        reset_keyboard();
-        return false;
-        break;
-     }
-  }
-  return true;
-}
-
 #ifndef NO_ACTION_ONESHOT
-    #define OS_LSFT OSM(MOD_LSFT)
-    #define OS_LALT OSM(MOD_LALT)
-    #define OS_LCTL OSM(MOD_LCTL)
-#else  //NO_ACTION_ONESHOT
-    #define OS_LSFT KC_LSFT
-    #define OS_LALT KC_LALT
-    #define OS_LCTL KC_LCTL
-#endif  //NO_ACTION_ONESHOT
+#    define OS_LSFT OSM(MOD_LSFT)
+#    define OS_LALT OSM(MOD_LALT)
+#    define OS_LCTL OSM(MOD_LCTL)
+#else // NO_ACTION_ONESHOT
+#    define OS_LSFT KC_LSFT
+#    define OS_LALT KC_LALT
+#    define OS_LCTL KC_LCTL
+#endif // NO_ACTION_ONESHOT
