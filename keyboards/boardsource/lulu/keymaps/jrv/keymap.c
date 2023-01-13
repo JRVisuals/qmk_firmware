4c868aef4bf821c1643950f7d2417b966a8d3fd4
jrod-disco <jrod.disco@gmail.com>
2023-01-13 17:53:04 -0500

    lulu: tweak gaming layout

--- keyboards/boardsource/lulu/keymaps/jrv/keymap.c
+++ keyboards/boardsource/lulu/keymaps/jrv/keymap.c
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GRV  ,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                              KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
   KC_TAB ,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                              KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    DBL_ITERM,
   GUI_ESC,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                              KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, CTRL_QUOT,
-  SFT_CAP ,   KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,  RGB_TOG,  RGB_TOG,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  SFT_ENT,
+  SFT_CAP ,   KC_Z,   KC_X,    KC_C,  KC_V,    KC_B,  RGB_TOG,  RGB_TOG,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  SFT_ENT,
                             KC_LALT, KC_LCTL,   LOWER,  SPACEFN    ,  SPACEFN,      RAISE,  KC_RGUI, KC_RCTL
 ),
 
@@ -146,11 +146,11 @@ _______, _______, _______, _______, _______, RGB_HUI,    RGB_MOD, RGB_TOG,      
  *                          `---------------------'           '------''-------------'
  */
  [_GAMING] = LAYOUT( \
-  KC_ESC, _______,    _______,    _______,    _______,    _______,                     _______,    _______,    _______,    _______,    _______,    _______,\
+  GUI_ESC, _______,    _______,    _______,    _______,    _______,                     _______,    _______,    _______,    _______,    _______,    _______,\
   KC_TAB, _______, _______, _______,  _______,    _______,                     _______,    _______,    _______,    _______,    _______,    _______, \
   KC_LSFT, _______,    _______,    _______,    _______,    _______,                     _______,    _______,    _______,    _______,    _______,    _______, \
   KC_LCTL, _______, _______,    _______,    _______,    _______, _______,  _______,  _______,    _______,    _______,    _______,    _______,    _______, \
-                            KC_LGUI, KC_LALT, KC_SPC,  KC_SPC,   KC_SPC,  KC_SPC, KC_LALT, KC_RCTL \
+                            KC_LALT, KC_LCTL, KC_SPC,  KC_SPC,   SPACEFN,  LOWER, RAISE, SPACEFN \
 )
 };
 
